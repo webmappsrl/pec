65921939a0d7a6982995dc60c2c6076adb049c6d
Pedram Katanchi <pedramkat@gmail.com>
2022-04-04 18:00:06 +0200

    Added responsive to taxonomy page maps

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,11 +1,14 @@
 @props(['trigger','last'])
 @php
+    use Jenssegers\Agent\Agent;
+    $agent = new Agent();
+
     $dropdown_classe = 'relative md:absolute w-full origin-top-right md:shadow-lg z-10 ';   
     
     if ($last) $dropdown_classe .= 'last-menu-item';
 @endphp
 <div @mouseover.away="open = false" class="relative" x-data="{ open: false }">
-    <button @mouseover="open = !open"  :class="{'text-primary bg-white md:text-white md:bg-primary': open, 'text-primary bg-white': !open}" class="inline-grid items-center grid-flow-col cursor-pointer text-primary px-6 py-2 md:hover:text-white md:hover:bg-primary">
+    <button @if ($agent->isMobile()) @click="open = !open" @else @mouseover="open = !open" @endif  :class="{'text-primary bg-white md:text-white md:bg-primary': open, 'text-primary bg-white': !open}" class="inline-grid items-center grid-flow-col cursor-pointer text-primary px-6 py-2 md:hover:text-white md:hover:bg-primary">
       {{ $trigger }}
       <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="pointer-events-none inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </button>
