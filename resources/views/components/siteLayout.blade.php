3dc4cd14779142412101eb33966e2353e94fe6e0
Pedram Katanchi <pedramkat@gmail.com>
2022-04-05 18:04:21 +0200

    Added google Analytics tag

--- resources/views/components/siteLayout.blade.php
+++ resources/views/components/siteLayout.blade.php
@@ -7,7 +7,17 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Itinerari - Parchi Emilia Centrale</title>
-    {{-- privacy iubenda --}}
+    
+    <!-- Global site tag (gtag.js) - Google Analytics --> 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-678825-51"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-678825-51');
+    </script>
+
+    <!-- privacy iubenda -->
     <script type="text/javascript">
     var _iub = _iub || [];
     _iub.csConfiguration = {"ccpaAcknowledgeOnDisplay":true,"ccpaApplies":true,"consentOnContinuedBrowsing":false,"enableCcpa":true,"floatingPreferencesButtonDisplay":"bottom-right","invalidateConsentWithoutLog":true,"perPurposeConsent":true,"siteId":2599802,"whitelabel":false,"cookiePolicyId":80094272,"lang":"it", "banner":{ "acceptButtonDisplay":true,"closeButtonRejects":true,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-bottom-left","rejectButtonDisplay":true }};
