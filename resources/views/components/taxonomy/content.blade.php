1ec976cf4b08b3e3dcf7e4f9f0f8b9d83b654275
Pedram Katanchi <pedramkat@gmail.com>
2022-09-30 12:52:33 +0200

    fix for empty maps

--- resources/views/components/taxonomy/content.blade.php
+++ resources/views/components/taxonomy/content.blade.php
@@ -4,12 +4,14 @@
         <div class="pb-8 taxonomy-body">
             {!! $taxonomy->description !!}
         </div>
-        <h3>{{$taxonomy->name}}: {{$tracks->total()}} Itinerari</h3>
-        <div class="grid md:grid-cols-3 overflow-x-scroll md:overflow-auto grid-flow-col md:grid-flow-row">
-            @foreach ($tracks as $feature)
-                <x-track-card :feature="$feature" :highlights="false"/>
-            @endforeach
-        </div>
-        {{ $tracks->links() }}
+        @if (count($tracks) > 0)
+            <h3>{{$taxonomy->name}}: {{$tracks->total()}} Itinerari</h3>
+            <div class="grid md:grid-cols-3 overflow-x-scroll md:overflow-auto grid-flow-col md:grid-flow-row">
+                @foreach ($tracks as $feature)
+                    <x-track-card :feature="$feature" :highlights="false"/>
+                @endforeach
+            </div>
+            {{ $tracks->links() }}
+        @endif
     </div>
 </div>
